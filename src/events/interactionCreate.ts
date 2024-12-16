f65771accb10c5d2476a5f43dfeac6bf12761894
shadyrajaab <shadyrajaab@gmail.com>
2024-12-16 18:50:24 -0300

    fix: resolver bug de não executar o comando

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,12 +1,16 @@
+import { Command } from "@interfaces";
 import { Client, Interaction } from "discord.js";
 
-export const interactionCreate = async (client: Client, commands: Object) => {
+export const interactionCreate = async (client: Client, interactions: Object) => {
     client.on("interactionCreate", async (interaction: Interaction) => {
         if (interaction.isCommand()) {
             const { commandName } = interaction;
+            const interactionCommand = interactions[commandName]
 
-            const execute = commands[commandName]
-            await execute(interaction)        
+            if (!interactionCommand) return;
+
+            const command = await interactionCommand.getCommand(client) as Command
+            await command.execute(interaction)        
         };
     })
 };
